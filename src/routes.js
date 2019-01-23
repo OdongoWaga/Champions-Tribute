283aca0399bffc23cd6f200891c1097af70c77e8
Waga Odongo <wagaodongo@gmail.com>
2019-01-23 20:13:10 +0300

    updated
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Routes extends Component {
+  render() {
+    return (
+      <div className="App">
+        hi
+      </div>
+    );
+  }
+}
+
+export default Routes;
