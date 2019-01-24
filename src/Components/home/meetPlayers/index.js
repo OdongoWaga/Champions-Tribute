ee7424ca0470649a3c268b10812ce94fb78400db
Waga Odongo <wagaodongo@gmail.com>
2019-01-24 16:46:12 +0300

    meet the players done
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Stripes from '../../../Resources/images/stripes.png'
+import { Tag } from '../../../utils/Misc';
 
  class MeetPlayers extends Component {
 
@@ -10,8 +11,85 @@ import Stripes from '../../../Resources/images/stripes.png'
   render() {
     return (
       <div className ="home_meetplayers"
-      style={{ background: `#ffffff`}}
+      style={{ background: `#ffffff url(${Stripes})`}}
       >
+      <div className="container">
+      <div className="home_meetplayers_wrapper">
+      <div className="home_card_wrapper">
+      card
+      </div>
+      <div className ="home_text_wrapper">
+      <div>
+        <Tag
+        bck="#0e1731"
+        size="100px"
+        color="#ffffff"
+        add={{
+            display:'inline-block',
+            marginBottom: '20px'
+        }}
+        >
+        Meet
+        </Tag>
+
+
+      </div>
+      <div>
+        <Tag
+        bck="#0e1731"
+        size="100px"
+        color="#ffffff"
+        add={{
+            display:'inline-block',
+            marginBottom: '20px'
+        }}
+        >
+        The
+        </Tag>
+
+
+      </div>
+      <div>
+        <Tag
+        bck="#0e1731"
+        size="100px"
+        color="#ffffff"
+        add={{
+            display:'inline-block',
+            marginBottom: '20px'
+        }}
+        >
+        Players
+        </Tag>
+
+
+      </div>
+      <div>
+          <div>
+            <Tag
+            bck="#ffffff"
+            size="27px"
+            color="#0e1731"
+            link
+            linkTo="/Team"
+            add={{
+                display:'inline-block',
+                marginBottom: '27px',
+                border:'1px solid #0e1731'
+            }}
+             
+            >
+
+                Meet the squad
+            </Tag>
+
+          </div>
+      </div>
+
+      </div>
+      </div>
+      </div>
+
         
       </div>
     )
