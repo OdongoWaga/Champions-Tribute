f4d4ceb3fdf0270023d1dd72e90a9b5eef4e37e2
Waga Odongo <wagaodongo@gmail.com>
2019-01-24 16:21:27 +0300

    meet_players
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ src/Components/home/meetPlayers/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Stripes from '../../../Resources/images/stripes.png'
+
+ class MeetPlayers extends Component {
+
+    state={
+
+    }
+
+  render() {
+    return (
+      <div className ="home_meetplayers"
+      style={{ background: `#ffffff`}}
+      >
+        
+      </div>
+    )
+  }
+}
+
+export default MeetPlayers;
