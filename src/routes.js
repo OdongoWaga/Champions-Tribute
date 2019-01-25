75a8affed30d87880a5afa86ef522224d3b1ab5f
Waga Odongo <wagaodongo@gmail.com>
2019-01-25 14:50:46 +0300

    AdminLayout
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Layout from './Components/HOC/Layout';
 import {Switch, Route} from "react-router-dom";
 import Home from './Components/home';
 import SignIn from './Components/signin';
+import Dashboard from './Components/admin/Dashboard';
 
 const Routes = (props) => {
   return (
@@ -11,6 +12,7 @@ const Routes = (props) => {
         <Switch>
           <Route exact path = "/sign_in" component={SignIn} />
           <Route exact path = "/" component={Home} />
+          <Route exact path = "/dashboard" component={Dashboard} />
         </Switch>
     
         </Layout>
